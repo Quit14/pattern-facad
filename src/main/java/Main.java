979ca24c01afdd54cce786234ab53f256b0b0c47
Anastasiya Volkova <ahh.write@gmail.com>
2022-10-19 19:26:21 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.mult("1010", "100"));
+        System.out.println(binOps.sum("111110100", "110111"));
+    }
+}
